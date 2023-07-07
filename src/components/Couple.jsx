7daf47ddeb8804d5b67cdbee4fd02203c73eb394
ziauddinziya07 <ziauddinziya07@gmail.com>
2023-07-07 08:53:26 +0530

    first commit

---
+++ src/components/Couple.jsx
@@ -0,0 +1,237 @@
+import React from "react";
+import "../styles/Couple.css";
+
+function Couple() {
+    return (
+        <section id="couple">
+            <div className="couple-wrapper">
+                <h1 className="title-quote">
+                    Happy Couple Going To Be Engaged
+                </h1>
+
+                <div className="row">
+                    <div className="col-lg-5" data-aos="fade-up">
+                        <div
+                            id="carouselIndicators"
+                            className="carousel slide"
+                            data-interval="2000"
+                            data-ride="carousel"
+                        >
+                            <ol className="carousel-indicators">
+                                <li
+                                    data-target="#carouselIndicators"
+                                    data-slide-to="0"
+                                    className="active"
+                                ></li>
+                                <li
+                                    data-target="#carouselIndicators"
+                                    data-slide-to="1"
+                                ></li>
+                                <li
+                                    data-target="#carouselIndicators"
+                                    data-slide-to="2"
+                                ></li>
+                            </ol>
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <div className="img-wrapper">
+                                        <img
+                                            className="couple-img"
+                                            src="../assets/images/groom.png"
+                                            alt="couple_img"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <div className="img-wrapper">
+                                        <img
+                                            className="couple-img"
+                                            src="../assets/images/groom.png"
+                                            alt="couple_img"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <div className="img-wrapper">
+                                        <img
+                                            className="couple-img"
+                                            src="../assets/images/groom.png"
+                                            alt="couple_img"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <a
+                                className="carousel-control-prev"
+                                href="#carouselIndicators"
+                                role="button"
+                                data-slide="prev"
+                            >
+                                <span
+                                    className="carousel-control-prev-icon"
+                                    aria-hidden="true"
+                                ></span>
+                                <span className="sr-only">Previous</span>
+                            </a>
+                            <a
+                                className="carousel-control-next"
+                                href="#carouselIndicators"
+                                role="button"
+                                data-slide="next"
+                            >
+                                <span
+                                    className="carousel-control-next-icon"
+                                    aria-hidden="true"
+                                ></span>
+                                <span className="sr-only">Next</span>
+                            </a>
+                            <img
+                                className="flower groom-flower"
+                                src="../assets/images/golden-rose.png"
+                                alt="flower"
+                            />
+                        </div>
+                        <div className="info groom-info">
+                            <h3>
+                                Phatan Ameer Khan,
+                                {/* <sub>Silver Smith,</sub> */}
+                            </h3>
+                            <p className="mt-2 mt-lg-3">
+                                Professsion: Works at{" "}
+                                <strong>Hexaware Technology,</strong> Pune.
+                            </p>
+                            <p className="mt-2 mt-lg-3">
+                                S/O Janab{" "}
+                                <strong>Phatan Mohdin Khan Sahab,</strong>
+                            </p>
+                            <p className="mt-1 mt-lg-2">
+                                #10/254, Near Sultanpur Masjid,
+                            </p>
+                            <p className="mt-1 mt-lg-2">
+                                Buddekal Street, Adoni.
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        className="col-lg-2"
+                        data-aos="zoom-out "
+                        data-aos-delay="300"
+                    >
+                        <img
+                            className=" heart-mid"
+                            src="../assets/images/heart.png"
+                            alt="heart_img"
+                        />
+                    </div>
+                    <div className="col-lg-5" data-aos="fade-up">
+                        <div
+                            id="bride-carousel-indicators"
+                            className="carousel slide"
+                            data-interval="2000"
+                            data-ride="carousel"
+                        >
+                            <ol className="carousel-indicators">
+                                <li
+                                    data-target="#bride-carousel-indicators"
+                                    data-slide-to="0"
+                                    className="active"
+                                ></li>
+                                <li
+                                    data-target="#bride-carousel-indicators"
+                                    data-slide-to="1"
+                                ></li>
+                                <li
+                                    data-target="#bride-carousel-indicators"
+                                    data-slide-to="1"
+                                ></li>
+                            </ol>
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <div className="img-wrapper">
+                                        <img
+                                            className="couple-img"
+                                            src="../assets/images/bride.png"
+                                            alt="couple_img"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <div className="img-wrapper">
+                                        <img
+                                            className="couple-img"
+                                            src="../assets/images/bride.png"
+                                            alt="couple_img"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <div className="img-wrapper">
+                                        <img
+                                            className="couple-img"
+                                            src="../assets/images/bride.png"
+                                            alt="couple_img"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <a
+                                className="carousel-control-prev"
+                                href="#bride-carousel-indicators"
+                                role="button"
+                                data-slide="prev"
+                            >
+                                <span
+                                    className="carousel-control-prev-icon"
+                                    aria-hidden="true"
+                                ></span>
+                                <span className="sr-only">Previous</span>
+                            </a>
+                            <a
+                                className="carousel-control-next"
+                                href="#bride-carousel-indicators"
+                                role="button"
+                                data-slide="next"
+                            >
+                                <span
+                                    className="carousel-control-next-icon"
+                                    aria-hidden="true"
+                                ></span>
+                                <span className="sr-only">Next</span>
+                            </a>
+                            <img
+                                className="flower bride-flower"
+                                src="../assets/images/golden-rose.png"
+                                alt="flower"
+                            />
+                        </div>
+                        <div className="info bride-info">
+                            <h3>
+                                Panchi Umme Aiman,
+                                {/* <sub></sub> */}
+                            </h3>
+                            {/* <p className="mt-2 mt-lg-3">
+                                Professsion: <strong>SG Teacher, Adoni.</strong>
+                            </p> */}
+                            <p className="mt-2 mt-lg-3">
+                               Grand D/O Janab{" "}
+                                <strong>Panchi Abdul Subhan Sahab(Late).</strong>
+                            </p>
+                            {/* <p className="mt-1 mt-lg-2">
+                                Retd. Agriculture Depatment Officer,
+                            </p> */}
+                            {/* <p className="mt-1 mt-lg-2">
+                                H.No.: 17-110/8, Kishan Singh Street,
+                            </p> */}
+                            {/* <p className="mt-1 mt-lg-2">Adoni.</p> */}
+                            {/* <p className="mt-1 mt-lg-2">
+                                D/O <strong>Shaikh Nazeer Ahmed.</strong>
+                            </p> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Couple;
---
+++ src/components/InviteNote.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../styles/InviteNote.css";
+
+function InviteNote() {
+    return (
+        <section id="invite-note">
+            <div className="from">
+                <h5>An Invitation from:</h5>
+                <h2>Mr. & Mrs. Panchi Nazeer Ahmed</h2>
+
+                {/* <h5 className="address">
+                    W/O Al-Haaj Khanbu Abdul Aleem Sahab,
+                    <br />
+                    Retd. Teacher and Pesh Imam
+                </h5> */}
+                <h5 className="address mb-3">
+                    H.No.: 2/160, Bharpet,
+                    <br /> Adoni, Kurnool Dist.,
+                    <br /> AP - 518 301.
+                </h5>
+                <p className="address">
+                    Contact: <br />
+                    +91 77801 80731.
+                    {/* <br />
+                    +91 98856 34576. */}
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default InviteNote;
